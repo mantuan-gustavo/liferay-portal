0b92a3b13b28989d954779ae3eba79bae297b315
mantuan-gustavo <38540612+mantuan-gustavo@users.noreply.github.com>
2018-12-19 17:49:55 -0200

    Delete PortletExtenderConfiguration.java
    Dont need it anymore

--- modules/apps/frontend-js-portlet-extender/frontend-js-portlet-extender/src/main/java/com/liferay/frontend/js/portlet/extender/configuration/PortletExtenderConfiguration.java
+++
@@ -1,6 +0,0 @@
-package com.liferay.frontend.js.portlet.extender.configuration;
-
-import aQute.bnd.annotation.metatype.Meta;
-
-@Meta.OCD(id = "PortletExtenderConfiguration")
-public interface PortletExtenderConfiguration {}
